d023be22ce9f0b1f4da2b97ae67511127be42733
Josh Stephens <bsdtux@gmail.com>
2019-03-16 13:24:10 -0500

    Update docstring message

--- python/two-fer/two_fer.py
+++ python/two-fer/two_fer.py
@@ -2,7 +2,7 @@
 
 
 def two_fer(name: str = "you") -> str:
-    """Returns Two string
+    """Returns a two fer message
     :param name: Name of person that gets something
     :type name: str
     :returns: String with name of person that gets something
