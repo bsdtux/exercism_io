365c46f28dfa248d2c81d9616a61fac0f4e4a8ca
Josh Stephens <bsdtux@gmail.com>
2019-03-16 13:02:38 -0500

    Completed hello world module

--- python/hello-world/hello_world.py
+++ python/hello-world/hello_world.py
@@ -1,2 +1,9 @@
-def hello():
-    pass
+"""Hello World Module"""
+
+
+def hello() -> str:
+    """Returns str literal Hello, World!
+    :return: Hello, World! string literal
+    :rtype: str
+    """
+    return 'Hello, World!'
